6e2b456b3bc352a5e4bc8ccb2aa5cb363b74485e
janellca <janellca@student.21-school.ru>
2025-03-03 09:37:16 +0700

    add validate

--- internal/model/model.go
+++ internal/model/model.go
@@ -1,29 +1,9 @@
 package model
 
-import "errors"
-
-var (
-	ErrEmtpyURL = errors.New("url is empty")
-)
-
 type LongURL struct {
-	URL string `json:"long_url"`
+	URL string `json:"long_url" binding:"required"`
 }
 
 type ShortURL struct {
-	URL string `json:"short_url"`
-}
-
-func (u LongURL) Validate() error {
-	if u.URL == "" {
-		return ErrEmtpyURL
-	}
-	return nil
-}
-
-func (u ShortURL) Validate() error {
-	if u.URL == "" {
-		return ErrEmtpyURL
-	}
-	return nil
+	URL string `json:"short_url" binding:"required"`
 }
